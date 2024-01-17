a2c3d09f4a26b05f1e3a6209492af0e069030791
Neo Boikano Makoa <boikanomakoa30@gmail.com>
2024-01-17 14:47:46 +0200

    wip

--- server/configs/key_dev.js
+++ server/configs/key_dev.js
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 export default {
-    stringConnection: "mongodb+srv://boikanomakoa:eLVRkFdiEwG4DaEL@cluster0.6trs3ts.mongodb.net/typeform_assessment_db"
-}
+  stringConnection: process.env.API_KEY,
+};
